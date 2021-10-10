32e213aed2f4e4c9b7e27e4a1030a9970ce82c54
Aryan Agrawal <91491007+ChocoChip-Cookies@users.noreply.github.com>
2021-10-10 02:37:51 +0000

    login and signup facility with db

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from flask import Flask, request, render_template,url_for
-
+from flask import Flask, request, render_template,url_for,redirect
+import sqlite3
 
 app=Flask(__name__)
+conn=sqlite3.connect('docpres.db')
+cur=conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS docdetails (name TEXT,hospital TEXT, proficiency TEXT, email TEXT)")
+conn.commit()
+conn.close()
 
 
 
+def insertValDoc(name, hospital, proficiency, email):
+  conn=sqlite3.connect('docpres.db')
+  cur=conn.cursor()
+  cur.execute("INSERT INTO docdetails VALUES (?,?,?,?)",(name,hospital,proficiency,email))
+  conn.commit()
+  printTableDoc()
+  conn.close()
+
+def printTableDoc():
+  conn=sqlite3.connect('docpres.db')
+  cur=conn.cursor()
+  cur.execute("SELECT * FROM docdetails")
+  rows=cur.fetchall()
+  print(rows)
+  conn.close()
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -16,12 +36,57 @@ def login():
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
-#@app.route('/makeDoctor')
+@app.route('/makeDoctor',methods=['POST'])
 
-@app.route('/dashboard/<name>')
-def dashboard(name):
-    return render_template('dashboard.html',name=name)
+def makeDoctor():
+  conn=sqlite3.connect('docpres.db')
+  cur=conn.cursor()
+  name=request.form['name']
+  hospital=request.form['hospital']
+  proficiency=request.form['proficiency']
+  email=request.form['email']
+  email = email.split("@")[0]+email.split("@")[1].split('.')[0]
+  insertValDoc(name,hospital,proficiency,email)
+  
+  cur.execute("SELECT * FROM docdetails WHERE email=?",(email,))
+  rows=cur.fetchall()
+  print("PRINTING ROWS",rows)
+  return redirect(url_for('dashboard',email=email,page=1))
 
+@app.route('/dashboard/<page>/<email>')
+def dashboard(page,email):
+  conn=sqlite3.connect('docpres.db')
+  cur=conn.cursor()
+  
+  if page=="1" or page=="3":
+    #signup
+    print(email)
+    cur.execute("SELECT * FROM docdetails WHERE email=?",(email,))
+    rows=cur.fetchall()
+    print("PRINTING ROWS",rows)
+  elif page=="2":
+    email=email.split("@")[0]+email.split("@")[1].split('.')[0]
+    cur.execute("SELECT * FROM docdetails WHERE email=?",(email,))
+    rows=cur.fetchall()
+    if rows==[]:
+      return "No Such User"
+    print("PRINTING ROWS - LOGIN:", rows)
+  name=rows[0][0]
+  proficiency=rows[0][2]
+  hospital=rows[0][1]
+  return render_template('dashboard.html',email=email,name=name,proficiency=proficiency,hospital=hospital)
+
+
+
+@app.route('/newpres/<email>')
+def newpres(email):
+    return render_template('newpres.html',email=email)
+
+@app.route('/makepres/<email>',methods=['POST'])
+def makepres(email):
+  pname=request.form['pname']
+  print(pname)
+  return redirect(url_for('dashboard',email=email,page=1))
 
 if __name__=="__main__":
     app.run(debug=True,host='0.0.0.0',port='5000')
